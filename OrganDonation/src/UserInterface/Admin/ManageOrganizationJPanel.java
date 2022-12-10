d8a9fe73ee41c765d3f4c511b7545f2cbdbacfa1
Farida Fakhri <fakhri.f@northeastern.edu>
2022-12-10 17:15:35 -0500

    Add functionality to create and manage organization for an enterprise for admin role

--- OrganDonation/src/UserInterface/Admin/ManageOrganizationJPanel.java
+++ OrganDonation/src/UserInterface/Admin/ManageOrganizationJPanel.java
@@ -7,15 +7,9 @@ package UserInterface.Admin;
 import HealthCentre.DB4OUtil.DB4OUtil;
 import HealthCentre.EcoSystem;
 import HealthCentre.Enterprise.Enterprise;
-import static HealthCentre.Enterprise.Enterprise.EnterpriseType.CovidCentre;
-import static HealthCentre.Enterprise.Enterprise.EnterpriseType.Government;
-import static HealthCentre.Enterprise.Enterprise.EnterpriseType.Legal;
-import static HealthCentre.Enterprise.Enterprise.EnterpriseType.PlasmaBank;
+import static HealthCentre.Enterprise.Enterprise.EnterpriseType.Donation;
 import HealthCentre.Organization.Organization;
-import HealthCentre.Organization.Organization.CovidCentreType;
-import HealthCentre.Organization.Organization.GovernmentType;
-import HealthCentre.Organization.Organization.LegalType;
-import HealthCentre.Organization.Organization.PlasmaBankType;
+import HealthCentre.Organization.Organization.DonationType;
 import HealthCentre.Organization.Organization.Type;
 import HealthCentre.Organization.OrganizationInventory;
 import HomeScreens.TableFormat;
@@ -63,37 +57,11 @@ public class ManageOrganizationJPanel extends javax.swing.JPanel {
        // }
        
        System.out.println(enterprise.getEnterpriseType());
-       System.out.println(enterprise.getEnterpriseType().toString().equals(PlasmaBank.toString()));
-       if(enterprise.getEnterpriseType().toString().equals(Legal.toString())){
-        for(Organization.LegalType legalType: Organization.LegalType.values()){
-            if (legalType.getValue().equals(Organization.LegalType.LegalOfficer.getValue())){
-                organizationJComboBox.addItem(legalType);
-            }
-        }
-        }
        
-       else if(enterprise.getEnterpriseType().toString().equals(PlasmaBank.toString())){
-           System.out.println("1");
-        for(Organization.PlasmaBankType plasmaType: Organization.PlasmaBankType.values()){
-            System.out.println("1");
-            System.out.println(plasmaType.getValue());
-            System.out.println(Organization.PlasmaBankType.PlasmaBank.getValue());
-            if (plasmaType.getValue().equals(Organization.PlasmaBankType.PlasmaBank.getValue())){
-                organizationJComboBox.addItem(plasmaType);
-            }
-        }
-        }
-       else if(enterprise.getEnterpriseType().toString().equals(CovidCentre.toString())){
-        for(Organization.CovidCentreType covidType: Organization.CovidCentreType.values()){
-            if (covidType.getValue().equals(Organization.CovidCentreType.CovidCentre.getValue())){
-                organizationJComboBox.addItem(covidType);
-            }
-        }
-        }
-       else if(enterprise.getEnterpriseType().toString().equals(Government.toString())){
-        for(Organization.GovernmentType govtType: Organization.GovernmentType.values()){
-            if (govtType.getValue().equals(Organization.GovernmentType.Government.getValue())){
-                organizationJComboBox.addItem(govtType);
+       if(enterprise.getEnterpriseType().toString().equals(Donation.toString())){
+        for(Organization.DonationType donType: Organization.DonationType.values()){
+            if (donType.getValue().equals(Organization.DonationType.Donation.getValue())){
+                organizationJComboBox.addItem(donType);
             }
         }
         }
@@ -109,15 +77,18 @@ public class ManageOrganizationJPanel extends javax.swing.JPanel {
     }
     }
     private void populateTable(){
+        System.out.println("In populate table");
         DefaultTableModel model = (DefaultTableModel) organizationJTable.getModel();
         
         model.setRowCount(0);
         
         for (Organization organization : directory.getOrganizationList()){
+            System.out.println("In loop");
             Object[] row = new Object[2];
             row[0] = organization.getName();
             row[1] = organization.getRealName();
-            
+            System.out.println(row[0]);
+            System.out.println(row[1]);
             model.addRow(row);
         }
     }
@@ -147,7 +118,6 @@ public class ManageOrganizationJPanel extends javax.swing.JPanel {
         jLabel6 = new javax.swing.JLabel();
         nameSeparator4 = new javax.swing.JSeparator();
         enterpriseNameTextField1 = new javax.swing.JTextField();
-        jLabel7 = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(255, 153, 153));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -183,7 +153,7 @@ public class ManageOrganizationJPanel extends javax.swing.JPanel {
         organizationJTable.setRowHeight(30);
         jScrollPane1.setViewportView(organizationJTable);
 
-        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-120, 80, 760, 460));
+        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 80, 380, 460));
 
         jLabel2.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
@@ -263,7 +233,7 @@ public class ManageOrganizationJPanel extends javax.swing.JPanel {
         nameSeparator1.setForeground(new java.awt.Color(0, 0, 0));
         panel1.add(nameSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 350, 290, 10));
 
-        add(panel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1040, 80, 370, 460));
+        add(panel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 80, 370, 460));
 
         jPanel3.setBackground(new java.awt.Color(31, 31, 31));
         jPanel3.setPreferredSize(new java.awt.Dimension(926, 70));
@@ -278,16 +248,13 @@ public class ManageOrganizationJPanel extends javax.swing.JPanel {
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, 1656, Short.MAX_VALUE)
-                .addContainerGap())
+                .addGap(34, 34, 34)
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 950, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(696, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, 44, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, 70, Short.MAX_VALUE)
         );
 
         add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1680, -1));
@@ -295,19 +262,16 @@ public class ManageOrganizationJPanel extends javax.swing.JPanel {
         jLabel6.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(255, 255, 255));
         jLabel6.setText("Enterprise:");
-        add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 80, -1, -1));
+        add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 70, -1, -1));
 
         nameSeparator4.setForeground(new java.awt.Color(0, 0, 0));
-        add(nameSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 150, 250, 10));
+        add(nameSeparator4, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 130, 190, 30));
 
         enterpriseNameTextField1.setBackground(new java.awt.Color(0, 0, 0));
         enterpriseNameTextField1.setFont(new java.awt.Font("Arial", 1, 20)); // NOI18N
         enterpriseNameTextField1.setForeground(new java.awt.Color(255, 255, 255));
         enterpriseNameTextField1.setBorder(null);
-        add(enterpriseNameTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 110, 230, 30));
-
-        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/organizationnn.png"))); // NOI18N
-        add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 180, 240, 200));
+        add(enterpriseNameTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 100, 170, 30));
     }// </editor-fold>//GEN-END:initComponents
 
     private void addJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addJButtonActionPerformed
@@ -340,38 +304,32 @@ public class ManageOrganizationJPanel extends javax.swing.JPanel {
         }
 
 // Main Process after validation checks
-//        if(check1.equals("")){
-//        if(enterprise.getEnterpriseType().toString().equals(Legal.toString())){
-//            directory.createLegalOrganization((LegalType)organizationJComboBox.getSelectedItem(), txtOrgRealName.getText());
-//        }
-//        else if(enterprise.getEnterpriseType().toString().equals(Government.toString())){
-//            directory.createGovernmentOrganization((GovernmentType)organizationJComboBox.getSelectedItem(), txtOrgRealName.getText());
-//        }
-//        else if(enterprise.getEnterpriseType().toString().equals(PlasmaBank.toString())){
-//            directory.createPlasmaOrganization((PlasmaBankType)organizationJComboBox.getSelectedItem(), txtOrgRealName.getText());
-//        }
-//        else if(enterprise.getEnterpriseType().toString().equals(CovidCentre.toString())){
-//            directory.createCovidCentreOrganization((CovidCentreType)organizationJComboBox.getSelectedItem(), txtOrgRealName.getText());
-//        }
-//        else{
-//            
-//        Type type = (Type) organizationJComboBox.getSelectedItem();
-//        directory.createOrganization(type, txtOrgRealName.getText());
-//        }
-//        populateTable();
-//        
-//        
-//       JOptionPane.showMessageDialog(null, new JLabel("<html><h2><I>A new Organization</I><font color='green'> created </font><I>successfully!/I<></h2></html>"));
-//                   
-//        //JOptionPane.showMessageDialog(null, "Organization created successfully!");
-//        
-//        
-//        dB4OUtil.storeSystem(system);
-//        
-//        
-//        txtOrgRealName.setText("");
-//        organizationJComboBox.removeItem(organizationJComboBox.getSelectedItem());
-//        }
+        if(check1.equals("")){
+        if(enterprise.getEnterpriseType().toString().equals(Donation.toString())){
+            directory.createDonationOrganization((DonationType)organizationJComboBox.getSelectedItem(), txtOrgRealName.getText());
+        }
+ 
+        else{
+            
+        Type type = (Type) organizationJComboBox.getSelectedItem();
+        directory.createOrganization(type, txtOrgRealName.getText());
+        }
+        System.out.println("data getting stored");
+        populateTable();
+        System.out.println("data stored");
+        
+        
+       JOptionPane.showMessageDialog(null, new JLabel("<html><h2><I>A new Organization</I><font color='green'> created </font><I>successfully!/I<></h2></html>"));
+                   
+        //JOptionPane.showMessageDialog(null, "Organization created successfully!");
+        
+        
+        dB4OUtil.storeSystem(system);
+        
+        
+        txtOrgRealName.setText("");
+        organizationJComboBox.removeItem(organizationJComboBox.getSelectedItem());
+        }
     }//GEN-LAST:event_addJButtonActionPerformed
 
     private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
@@ -403,7 +361,6 @@ public class ManageOrganizationJPanel extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator nameSeparator;
